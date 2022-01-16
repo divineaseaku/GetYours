7a17234ed9a0342508ca75debfa8d71e93a85e95
Divine Aseaku <divineaseaku@gmail.com>
2022-01-15 23:58:40 -0500

    js and html

---
+++ main.js
@@ -0,0 +1,30 @@
+var sideBtn = document.querySelector('.side-nav-btn');
+var sideNav = document.querySelector('.main-menu');
+var sideBtn = document.querySelector('.side-btn');
+
+//event listener for click on side nav button
+sideBtn.addEventListener('click', function() {
+  if (sideNav.style.display === 'none') {
+    openNav();
+  } else {
+    // window.onclick=e => {
+    //   console.log(e.target);
+    //   console.log(e.target.tagName);
+    // }
+    closeNav();
+  }
+});
+
+
+//function for closing side nav
+function closeNav() {
+  sideNav.style.display = 'none';
+  sideBtn.style.color = '#fff';
+}
+
+
+//function for opening side nav
+function openNav() {
+  sideNav.style.display = 'block';
+  sideBtn.style.color = '#111';
+}
